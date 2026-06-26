65e79b2ca68391f290a6df8acf45dc0d6d2ef0c2
orlandotess <orlando@otesspr.com>
2026-06-26 10:52:46 -0400

    Remove middleware use client auth

--- fix_login.py
+++ fix_login.py
@@ -1,6 +1,3 @@
-old = 'sb_publishable_wL7A9THCYwVcyu3t6uk-3Q_Vt09bJzn'
-new = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inppc2lkb3J3ZGhydHRtZHBwbmJqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODI0MzA3NDEsImV4cCI6MjA5ODAwNjc0MX0.dKCf0omLnIy3AILNaU8vWj_yrMlJM-Fh9sOui71a7Po'
-with open('app/login/page.js','r') as f: content=f.read()
-content=content.replace(old,new)
-with open('app/login/page.js','w') as f: f.write(content)
-print('done')
+code = open('app/login/page.js').read()
+print('has onSubmit:', 'onSubmit' in code)
+print('has handleLogin:', 'handleLogin' in code)
