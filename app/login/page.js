72415d18a352bfdd162e03d39c87f48945d8a146
orlandotess <orlando@otesspr.com>
2026-06-26 10:40:31 -0400

    Fix login anon key

--- app/login/page.js
+++ app/login/page.js
@@ -4,7 +4,7 @@ import { createClient } from '@supabase/supabase-js';
 
 const supabase = createClient(
   'https://zisidorwdhrttmdppnbj.supabase.co',
-  'sb_publishable_wL7A9THCYwVcyu3t6uk-3Q_Vt09bJzn'
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inppc2lkb3J3ZGhydHRtZHBwbmJqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODI0MzA3NDEsImV4cCI6MjA5ODAwNjc0MX0.dKCf0omLnIy3AILNaU8vWj_yrMlJM-Fh9sOui71a7Po'
 );
 
 export default function LoginPage() {
