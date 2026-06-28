044a97222287d6093ec618ff6f450cdeedf5f89f
orlandotess <orlando@otesspr.com>
2026-06-28 11:56:25 -0400

    Add dynamic export and revalidation settings

--- app/trabajos/page.js
+++ app/trabajos/page.js
@@ -1,3 +1,5 @@
+export const dynamic = 'force-dynamic';
+export const revalidate = 0;
 import { supabase } from '../../lib/supabase';
 import Sidebar from '../Sidebar';
 import Link from 'next/link';
